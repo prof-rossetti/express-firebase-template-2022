ac118c9d970664f36ad41eb6f47630335bd6e70d
MJ Rossetti <s2t2@users.noreply.github.com>
2022-04-19 16:13:29 -0400

    New pages and routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+/* GET hello page. */
+router.get('/hello', function(req, res, next) {
+  console.log("URL PARAMS:", req.query)
+  var name = req.query.name || "World" // double pipes is an OR operator that allows us to use a default value if the url params are null / not specified
+  var message = "Hello, " + name
+  res.render('hello', { message: message });
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+
+  var users = [
+    {"id": 1, "name": "User 1", "email": "user1@example.com"},
+    {"id": 2, "name": "User 2", "email": "user2@yahoo.com"},
+    {"id": 3, "name": "User 3", "email": "user3@myschool.edu"},
+  ] // just some dummy data
+
+  res.send(users);
 });
 
 module.exports = router;
